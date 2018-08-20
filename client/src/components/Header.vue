5f9ad30e954e01010bf3d60ee0f8bb2df059eca8
Oliver Arthur <oliverarthurnardi@gmail.com>
2018-08-20 21:32:19 +0100

    create intro page

---
+++ client/src/components/Header.vue
@@ -0,0 +1,50 @@
+<template>
+  <header class="header">
+    <div class="header__inner">
+      <div class="header__inner-item">
+        Logo
+      </div>
+      <div class="header__inner-item nav-section">
+        <router-link to="" class="btn btn--primary-outline">Login</router-link>
+      </div>
+    </div>
+  </header>
+</template>
+
+<script>
+export default {
+  name: 'CHeader'
+}
+</script>
+
+<style lang="scss" scoped>
+  @import "../assets/scss/components/buttons/button";
+
+  .header {
+    background-color: $white;
+    height: 6.5rem;
+    position: fixed;
+    top: 0;
+    width: 100%;
+    z-index: 999;
+
+    &__inner {
+      column-gap: 1rem;
+      display: grid;
+      grid-template-columns: repeat(2, 1fr);
+      height: 6.5rem;
+    }
+
+    &__inner-item {
+      align-self: center;
+      font-size: 3rem;
+      font-weight: 200;
+      padding: 0 1.5rem;
+    }
+
+    .nav-section {
+      position: relative;
+      text-align: right;
+    }
+  }
+</style>
--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -1,14 +1,84 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
+    <c-header></c-header>
+    <div class="container">
+    <c-grid>
+      <c-grid-inner>
+        <c-grid-cell
+          align="middle"
+          :span-desktop="6"
+          :span-phone="12"
+          :span-tablet="12">
+          <div class="container--column">
+            <h1>Easy way to have your task and project organize</h1>
+            <h2>
+              Logo is project and task management software that any team
+              can use to collaborate and share note efficiently.
+            </h2>
+            <router-link to="" class="btn btn-primary">Get Started!</router-link>
+          </div>
+        </c-grid-cell>
+        <c-grid-cell
+          align="middle"
+          :span-desktop="6"
+          :span-phone="12"
+          :span-tablet="12">
+          <div class="container--column">
+            image
+          </div>
+        </c-grid-cell>
+      </c-grid-inner>
+    </c-grid>
+    </div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
+import CHeader from '@/components/Header'
+import CGrid from '@/components/Grid'
+import CGridInner from '@/components/GridInner'
+import CGridCell from '@/components/GridCell'
 
 export default {
   name: 'home',
-  components: {}
+  components: {
+    CHeader,
+    CGrid,
+    CGridInner,
+    CGridCell
+  }
 }
 </script>
+
+<style lang="scss" scoped>
+@import "../assets/scss/components/grid/grid";
+@import "../assets/scss/components/buttons/button";
+
+.container {
+  min-height: 100vh;
+
+  .grid, .grid__inner {
+    min-height: 100vh;
+  }
+
+  &--column {
+    h1, h2 {
+      line-height: 1.2;
+      margin-bottom: 1.6rem;
+    }
+
+    h1 {
+      color: $heading-font-color;
+      font-size: $heading-font-h1;
+      font-weight: $heading-font-weight;
+    }
+
+    h2 {
+      color: $heading-font-color;
+      font-size: $heading-font-h5-xs;
+      font-weight: $heading-font-weight;
+    }
+
+  }
+}
+</style>
