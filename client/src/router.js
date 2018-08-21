d7254b82c225cf91ef6f18fad53eb3163fdf47f5
Oliver Arthur <oliverarthurnardi@gmail.com>
2018-08-21 19:35:45 +0100

    create login

--- client/src/router.js
+++ client/src/router.js
@@ -32,9 +32,20 @@ const router = new Router({
       // route level code-splitting
       // this generates a separate chunk (about.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
-      component: () => import(/* webpackChunkName: "about" */ './views/SignUp.vue'),
+      component: () => import(/* webpackChunkName: "about" */ './views/Signup.vue'),
       meta: {
         title: 'Logo | Signup'
+      }
+    },
+    {
+      path: '/login',
+      name: 'login',
+      // route level code-splitting
+      // this generates a separate chunk (about.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: () => import(/* webpackChunkName: "about" */ './views/Login.vue'),
+      meta: {
+        title: 'Logo | Log in'
       }
     }
   ]
