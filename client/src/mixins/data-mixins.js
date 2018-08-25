689aa19e5e979c512c3ef5ff93a38c6c1344ccf2
Oliver Arthur <oliverarthurnardi@gmail.com>
2018-08-25 22:59:30 +0100

    create data mixins

---
+++ client/src/mixins/data-mixins.js
@@ -0,0 +1,38 @@
+export default {
+  data () {
+    return {
+      selected: 0,
+      selectedRole: null,
+      selectedUser: null,
+      showGroupForm: false,
+      showGroupUpdateForm: false,
+      showInviteUserForm: false,
+      showAddUsersToGroupForm: false,
+      groups: [{
+        name: 'All users'
+      }, {
+        name: 'Ungrouped'
+      }],
+      usersOverviews: [{
+        name: 'Administrators',
+        filter: ['Owner', 'Administrator']
+      }, {
+        name: 'Regular Users',
+        filter: ['Regular User']
+      }, {
+        name: 'Collaborators',
+        filter: ['Collaborator']
+      }, {
+        name: 'External Users',
+        filter: ['External User']
+      }],
+      getUser: {},
+      getUsers: [],
+      getGroups: [],
+      styleObj: {
+        width: `${window.innerWidth}px`,
+        height: `${window.innerHeight - 52}px`
+      }
+    }
+  }
+}
