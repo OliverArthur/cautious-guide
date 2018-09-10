98f50242f289dc75f954d292beb18ec97fcd00b8
Oliver Arthur <OliverArthur@users.noreply.github.com>
2018-09-10 21:54:25 +0100

    Create task tree (#44)
    * WIP
    
    * move scss to a separete file
    
    * change scss variable color

---
+++ client/src/components/Task.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="task">
+    <div class="task__header">
+      <span>Title</span>
+      <span>Status</span>
+      <span>Assigned to</span>
+      <span>Created</span>
+    </div>
+    <ul class="project__list">
+      <task-list
+        v-for="item in taskData"
+        :key="item.id"
+        :task="item">
+      </task-list>
+    </ul>
+  </div>
+</template>
+
+<script>
+import TaskList from '@/components/Tasklist'
+
+export default {
+  name: 'Task',
+  props: {
+    taskData: {
+      type: Array,
+      require: true
+    },
+  },
+  components: {
+    TaskList
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+@import "../assets/scss/components/task/task";
+</style>
+
