266ba092b50500fb4c14b0325a71b790908cb60a
Oliver Arthur <oliverarthurnardi@gmail.com>
2018-09-11 08:30:41 +0100

    fix the method the get token

--- server/src/utils.js
+++ server/src/utils.js
@@ -2,10 +2,10 @@ const jwt = require('jsonwebtoken')
 require('dotenv').config()
 
 function getUserId(context) {
-  const Authorization = context.request.get('Authorization')
+  const Authorization = context.req.headers.autherizantion
   if (Authorization && Authorization.length) {
-    const token = Authorization.replace('Bearer ', '')
-    const { id } = jwt.verify(token, process.env.JWT_SECRET)
+    const token = Authorization.replace('user-token ', '')
+    const id = jwt.verify(token, process.env.JWT_SECRET)
     return id;
   }
 
